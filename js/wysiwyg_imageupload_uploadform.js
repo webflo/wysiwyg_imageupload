20674ac5548a0d74b290a75deab91862dc729171
Eugen Mayer <eugen.mayer@kontextwork.de>
2010-06-24 05:01:39 +0200

    adding utf8 headers

--- js/wysiwyg_imageupload_uploadform.js
+++ js/wysiwyg_imageupload_uploadform.js
@@ -1,8 +1,6 @@
 // $Id:$
 // Copyright (c) 2010 KontextWork GbR
 // Author: Eugen Mayer
-
-
 Drupal.behaviors.wysiwyg_imageupload_uploadform= function () {
   $('#edit-wysiwyg-imageupload-file').bind('change', function() {
       $('.dialog-page-content *').hide();
